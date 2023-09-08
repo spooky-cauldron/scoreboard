99a0b22e66419f8b0ab9ac5a67d7e4076a7ad0d9
spooky-cauldron <spookycauldron0@gmail.com>
2023-09-07 21:44:35 -0600

    Added board id path parameter to scoreboard endpoints.

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import logging
+from uuid import uuid4
 
 import redis
 from flask import Flask
-from pydantic import BaseModel, Field
 
 from middleware import body
+from schema import PostScoreboardBody, PostScoreboardResponse
 
 app = Flask('scoreboard')
 
@@ -13,29 +14,31 @@ log = logging.getLogger()
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 
-class PostScoreboardBody(BaseModel):
-    name: str = Field(min_length=1, max_length=1024)
-    teams: list[str] = Field(min_length=1, max_length=1024)
+@app.get('/scoreboard/<board_id>')
+def get_board_info(board_id: str):
+    return r.hgetall(f'board:{board_id}:info')
 
 
-@app.get('/scoreboard')
-def get_board_info():
-    return r.hgetall('board:info')
+@app.get('/scoreboard/<board_id>/data')
+def get_board_data(board_id: str):
+    return r.hgetall(f'board:{board_id}:data')
 
 
-@app.get('/scoreboard/data')
-def get_board_data():
-    return r.hgetall('board:data')
+@app.get('/scoreboard/<board_id>/data/<team>')
+def get_board_team_score(board_id: str, team: str):
+    return r.hget(f'board:{board_id}:data', team)
 
 
 @app.post('/scoreboard')
 @body(PostScoreboardBody)
 def post_board(body: PostScoreboardBody):
+    board_id = str(uuid4())
     board_info = {'name': body.name}
     board_data = {team: 0 for team in body.teams}
-    r.hset('board:info', mapping=board_info)
-    r.hset('board:data', mapping=board_data)
-    return body.model_dump()
+    r.hset(f'board:{board_id}:info', mapping=board_info)
+    r.hset(f'board:{board_id}:data', mapping=board_data)
+    response = PostScoreboardResponse(id=board_id, **body.model_dump())
+    return response.model_dump()
 
 
 if __name__ == '__main__':
---
+++ schema.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field
+
+
+class PostScoreboardBody(BaseModel):
+    name: str = Field(min_length=1, max_length=1024)
+    teams: list[str] = Field(min_length=1, max_length=1024)
+
+
+class PostScoreboardResponse(PostScoreboardBody):
+    id: str
